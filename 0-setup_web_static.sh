9ad7cb3d5a5bb0ab319c05bbb669258ced6163d1
mamesareagan <mamesareagan0@gmail.com>
2023-10-05 21:12:49 +0000

    modified

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,19 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Installs Nginx if not installed
+#+ Creates folders /data/web_static/shared
+#+	and /adta/releases/test if not exists
+#+ /data/web_static/current linked to
+#+ /data/web_static/releases/test/
+#+ Creates an /data/web_static/releases/test/index.html
+#+ Configures Nginx to serve /data/web_static/current/ 
+#+	to hbnb_static
 
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Hello World!" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://github.com/mamesareagan0/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
