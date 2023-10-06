e7b8184366e1f491516357356ae1d400ead0cb07
mamesareagan <mamesareagan0@gmail.com>
2023-10-06 20:38:54 +0000

    deploy web statis

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,48 @@
 #!/usr/bin/python3
-"""
-    Distributes an archive to your web servers,
-    using the function do_deploy
-    def do_deploy(archive_path):
-    Return False iff archive path doesn't exist
-"""
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-from fabric.api import put, run, env, sudo
-from os.path import exists
-env.hosts = ['100.25.165.225', '18.233.65.83']
+env.hosts = ["100.25.165.225", "18.233.65.83"]
 
-#run("rm -rf /data/web_static/releases/web_static_20230506221613/images")
-#run("rm -rf /data/web_static/releases/web_static_20230506221613/styles")
 
 def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    Deploys an archive to a server
-    """
-    if exists(archive_path) is False:
+    if os.path.isfile(archive_path) is False:
         return False
-    try:
-        archive_name = archive_path.split("/")[-1]
-        no_ext = archive_name.split(".")[0]
-        path_no_ext = "/data/web_static/releases/" + no_ext
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-        put(archive_path, "/tmp/")
-        sudo("mkdir -p {}".format(path_no_ext))
-        sudo("tar -xzf /tmp/{} -C {}/".format(archive_name, path_no_ext))
-        sudo("rm /tmp/{}".format(archive_name))
-        sudo("mv {}/web_static/* {}/".format(path_no_ext, path_no_ext))
-        sudo("rm -rf {}/web_static".format(path_no_ext))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s {}/ /data/web_static/current".format(path_no_ext))
-        sudo("chmod -R 755 {}".format(path_no_ext))
-
-        print("New version deployed!")
-        return True
-    except FileNotFoundError:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
