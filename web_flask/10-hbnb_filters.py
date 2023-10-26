25e96164ccf22a34ce4ccafea7759cd8f763164d
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-10-26 05:40:04 -0700

    web_flask

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+from flask import Flask, render_template
+
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(err):
+    """teardown app context"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0")
